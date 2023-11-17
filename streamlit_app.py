ae2f5f4b29284912772955453be756938cd2c0a0
Anuj Verma <anuj.mca.iitd@gmail.com>
2023-11-17 21:05:18 +0530

    following snowflake badge 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,21 +1,33 @@
+'''
+My First Streamlit App depoyed on cloud
+'''
 import streamlit
 import pandas
+import requests
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
-streamlit.header('Breakfast Favorites')
-streamlit.text(' Omega 3 & Blueberry Oatmeal')
-streamlit.text(' Kale, Spinach & Rocket Smoothie')
-streamlit.text(' Hard-Boiled Free-Range Egg')
-streamlit.text('0 Avocado Toast')
+streamlit.header("Breakfast Menu")
+streamlit.text("Omega 3 & Blueberry Oatmeal")
+streamlit.text("Kale, Spinach & Rocket Smoothie")
+streamlit.text("Hard-Boiled Free-Range Egg")
+streamlit.header("Breakfast Favorites")
+streamlit.text(" Omega 3 & Blueberry Oatmeal")
+streamlit.text(" Kale, Spinach & Rocket Smoothie")
+streamlit.text(" Hard-Boiled Free-Range Egg")
+streamlit.text("0 Avocado Toast")
 
-streamlit.header('&3 Build Your Own Fruit Smoothie')
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
+streamlit.header("&3 Build Your Own Fruit Smoothie")
+my_fruit_list = pandas.read_csv(
+    """https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt"""
+)
+my_fruit_list = my_fruit_list.set_index("Fruit")
 
-fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
+fruits_selected = streamlit.multiselect(
+    "Pick some fruits:", list(my_fruit_list.index), ["Avocado", "Strawberries"]
+)
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
+
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
