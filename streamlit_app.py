2c658a842ae323f4f531e53492b7156b5d2a4578
Anuj Verma <anuj.mca.iitd@gmail.com>
2023-11-17 23:57:49 +0530

    following snowflake badge 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,15 +57,22 @@ except URLError as e:
 # # output the normalized data
 # streamlit.dataframe(fruityvice_normalized)
 
+
+
+# snowflake related functions
+def get_fruid_load_list():
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
+        return my_cur.fetchall()
+
+# add a button to load the fruit
+if streamlit.button("Get Fruit Load List"):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_fruid_load_list()
+    streamlit.dataframe(my_data_rows)
+
+
 streamlit.stop()
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
-
 
 # what fruit would you like to add to the list?
 fruit_to_add = streamlit.text_input("What fruit would you like to add to the list?", 'jackfruit')
