9df8b7c39e338404770c5ea5e89c41bd302c566e
Anuj Verma <anuj.mca.iitd@gmail.com>
2023-11-17 17:33:24 +0530

    following snowflake badge 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,17 @@
 import streamlit
 import pandas
 
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('Breakfast Favorites')
+streamlit.text(' Omega 3 & Blueberry Oatmeal')
+streamlit.text(' Kale, Spinach & Rocket Smoothie')
+streamlit.text(' Hard-Boiled Free-Range Egg')
+streamlit.text('0 Avocado Toast')
 
+streamlit.header('&3 Build Your Own Fruit Smoothie')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
