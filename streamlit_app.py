b822065ffafc22943d73d0867d8f9d74b45f7d52
Anuj Verma <anuj.mca.iitd@gmail.com>
2023-11-17 23:42:03 +0530

    following snowflake badge 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,11 +32,21 @@ streamlit.dataframe(fruits_to_show)
 
 
 streamlit.header("Fruityvice Fruit Advice!")
+try:
+    fruit_choice = streamlit.text_input("What fruit would you like information about?", 'Kiwi')
+    if not fruit_choice:
+        streamlit.error('Please select a fruit to get information.')
+    else:
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
-fruit_choice = streamlit.text_input("What fruit would you like information about?", 'Kiwi')
-streamlit.write('The user entered', fruit_choice)
+        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+        streamlit.dataframe(fruityvice_normalized)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+
+except URLError as e:
+    streamlit.error("URL error: " + str(e.reason))
+
+
 
 # take the json response and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
