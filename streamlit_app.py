4c49020402780844f54aa636e1bac051a874e4fe
Anuj Verma <anuj.mca.iitd@gmail.com>
2023-11-18 00:05:16 +0530

    following snowflake badge 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -74,9 +74,15 @@ if streamlit.button("Get Fruit Load List"):
 
 streamlit.stop()
 
-# what fruit would you like to add to the list?
-fruit_to_add = streamlit.text_input("What fruit would you like to add to the list?", 'jackfruit')
-streamlit.write('Thanks for adding ', fruit_to_add)
+# Allow the end user to add a fruit to the list
+def insert_row_snowflake(new_fruit):
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("INSERT INTO FRUIT_LOAD_LIST VALUES ('from streamlit')")
+        return "Thanks for adding " + new_fruit
 
-# This will not work correctly
-my_cur.execute("INSERT INTO FRUIT_LOAD_LIST VALUES ('from streamlit')")
+add_my_fruit = streamlit.text_input("What fruit would you like to add?")
+
+if streamlit.button("Add Fruit"):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    back_from_function = insert_row_snowflake(add_my_fruit)
+    streamlit.text(back_from_function)
