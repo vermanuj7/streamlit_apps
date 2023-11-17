2a111b31cf13823f87d1c9c7d0dbeb69dcd58fe2
Anuj Verma <anuj.mca.iitd@gmail.com>
2023-11-17 23:26:40 +0530

    following snowflake badge 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,3 +49,8 @@ my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+
+# what fruit would you like to add to the list?
+fruit_to_add = streamlit.text_input("What fruit would you like to add to the list?", 'jackfruit')
+streamlit.write('Thanks for adding', fruit_to_add)
