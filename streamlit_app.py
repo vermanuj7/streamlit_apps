e646016d3ae6a0838400be5e267d2df0ee8129b4
Anuj Verma <anuj.mca.iitd@gmail.com>
2023-11-18 00:42:32 +0530

    following snowflake badge 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -76,12 +76,11 @@ add_my_fruit = streamlit.text_input("What fruit would you like to add?")
 
 # add a button to load the fruit
 streamlit.header("View our Fruit List - Add your Favourites!")
-# if streamlit.button("Get Fruit List"):
-#     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-#     my_data_rows = get_fruid_load_list()
-#     my_cnx.close()
-#     streamlit.dataframe(my_data_rows)
 if streamlit.button("Get Fruit List"):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-    back_from_function = insert_row_snowflake(add_my_fruit)
-    streamlit.text(back_from_function)
+    my_data_rows = get_fruid_load_list()
+    my_cnx.close()
+    streamlit.dataframe(my_data_rows)
+    # my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    # back_from_function = insert_row_snowflake(add_my_fruit)
+    # streamlit.text(back_from_function)
